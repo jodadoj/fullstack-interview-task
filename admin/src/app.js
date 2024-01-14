bc9873b52361bfe5dcf07a317b453789ace60cdf
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2024-01-14 14:38:28 +0000

    changed company data array to object

--- admin/src/app.js
+++ admin/src/app.js
@@ -16,13 +16,13 @@ const getInvestment = R.pipeWith(
 
 const getCompany = R.pipeWith(
   (func, input) => Promise.resolve(input).then(func), 
-  [R.pipe(R.prop('id'), fetchCompany
-  // , getNameID
-  )]
+  [R.pipe(R.prop('id'), fetchCompany)]
 )
 
 function getNameID(company){
-  return {id: company.id ?? null, name: company.name ?? null}
+  console.table(company)
+  console.table({id: company.id, name: company.name})
+  return {id: company.id, name: company.name}
 }
 
 function fetchInvestment(id){
@@ -44,6 +44,7 @@ function fetchInvestment(id){
 }
 
 function fetchCompany(id){
+  console.log(`given id was ${id}`)
   return new Promise((resolve, reject) => {
     request.get(
     {
@@ -61,16 +62,7 @@ function fetchCompany(id){
 })
 }
 
-app.get("/investments/:id", async (req, res) => {
-  userLogger.log("info", "Begining to attempt to fetch investment data");
-  const investments = await getInvestment(req)
-
-  const companyData = []
-
-  for (const holding of investments.holdings){
-    const company = await getCompany(holding)
-    companyData.push(company)
-  }
+function sendCSV(investments, companyData){
   const headers = (
     "User,FirstName,LastName,Date,Holding,Value\n"
   )
@@ -87,6 +79,59 @@ app.get("/investments/:id", async (req, res) => {
       `${investments.lastName},` +
       `${investments.date},` +
       `${companyData[holding].name},` + 
+      `${investmentValue}\n`
+    )
+
+    rowStrings.push(holdingCSV)
+  }
+
+  const exportCSVBody = [headers, rowStrings.join("")].join("")
+
+  request.post(
+  {
+    url: `${config.investmentsServiceUrl}/investments/export`, 
+    json: true,
+    body: {csv: exportCSVBody}
+  }, (e, r, body) => {
+    if (e) {
+      console.error(e)
+      res.send(500)
+    }
+  })
+
+  return {csv: exportCSVBody}
+}
+
+app.get("/investments/:id", async (req, res) => {
+  userLogger.log("info", "Begining to attempt to fetch investment data");
+  const investments = await getInvestment(req)
+
+  const companyData = {}
+
+  for (const holding of investments.holdings){
+    console.table(holding)
+    const company = await getCompany(holding)
+    console.table(company)
+    companyData[company.id] = company.name
+  }
+
+  console.table(companyData)
+  const headers = (
+    "User,FirstName,LastName,Date,Holding,Value\n"
+  )
+  const rowStrings = []
+
+  for (const holding in investments.holdings){
+    const currentHolding = investments.holdings[holding]
+    const holdingPercentage = currentHolding.investmentPercentage
+    const investmentValue = holdingPercentage * investments.investmentTotal
+
+    const holdingCSV = (
+      `${investments.userId},` +
+      `${investments.firstName},` +
+      `${investments.lastName},` +
+      `${investments.date},` +
+      `${companyData[currentHolding.id]},` + 
       `${investmentValue}\n`
     )
 
