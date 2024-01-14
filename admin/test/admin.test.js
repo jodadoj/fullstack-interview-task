a3776337aaa6377543dd174531fa96a989472a00
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2024-01-14 17:23:27 +0000

    Cleaned up error test

--- admin/test/admin.test.js
+++ admin/test/admin.test.js
@@ -2,31 +2,23 @@ const request = require("request");
 const config = require("config");
 const { fetchInvestment } = require("../src/app");
 
-jest.mock("request", () => ({ get: jest.fn() }));
-jest.mock("config", () => ({ investmentsServiceUrl: "http://localhost:8081" }));
+jest.mock("request", () => ({
+  get: jest.fn(),
+}));
+jest.mock("config", () => ({
+  investmentsServiceUrl: "",
+  financialCompaniesServiceUrl: "",
+}));
 
 describe("Fetch investment", () => {
   it("Gets the data of a given investment via a given id", () => {
     //arrange
-    const expectedResponse = {
-      id: "2",
-      userId: "2",
-      firstName: "Sheila",
-      lastName: "Aussie",
-      investmentTotal: 20000,
-      date: "2020-01-01",
-      holdings: [
-        { id: "1", investmentPercentage: 0.5 },
-        { id: "2", investmentPercentage: 0.5 },
-      ],
-    };
+    const expectedResponse = {};
+    const givenId = 0;
     //act
     request.get.mockImplementation(async () => {
       callback(expectedResponse);
-
-      const givenId = 2;
       const fetchResponse = await fetchInvestment(givenId);
-
       //assert
       expect(fetchResponse).toEqual(expectedResponse);
     });
@@ -34,14 +26,12 @@ describe("Fetch investment", () => {
   it("Returns an error when issues occur during the request", () => {
     //arrange
     const mockedError = "boo";
-      //act
-      request.get.mockRejectedValue(async () => {
-        callback(new Error(mockedError));
-  
-        const givenId = 2;
-  
-        //assert
-        expect(fetchInvestment(givenId)).rejects.toThrow(mockedError);
-      });
-  })
+    const givenId = 0;
+    //act
+    request.get.mockRejectedValue(async () => {
+      callback(new Error(mockedError));
+      //assert
+      expect(fetchInvestment(givenId)).rejects.toThrow(mockedError);
+    });
+  });
 });
